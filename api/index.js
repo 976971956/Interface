0a3610d19cab4211aea1220222c6c7326e77a715
UJF <976971956@qq.com>
2025-08-15 09:33:33 +0800

    fix: correct Vercel function export for serverless-http

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('../app');
 
-module.exports = (req, res) => {
-	const handler = serverless(app);
-	return handler(req, res);
-}; 
+const handler = serverless(app);
+
+module.exports = handler; 
